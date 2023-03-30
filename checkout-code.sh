3e0d216c9f3f6cd557b5eabf8b8e4db5b8a707a5
arun.alagesan <arun.alagesan@gmail.com>
2023-03-30 17:36:48 +0530

    Added calander service to the build and deployment

--- checkout-code.sh
+++ checkout-code.sh
@@ -12,5 +12,7 @@ echo $SMS_API_SOURCE
 git clone  -b main --single-branch  $SMS_API_SOURCE 
 echo $SMS_UI_SOURCE
 git clone  -b master --single-branch  $SMS_UI_SOURCE 
+echo $CAL_API_SOURCE
+git clone  -b main --single-branch  $CAL_API_SOURCE 
 cd ..
 
