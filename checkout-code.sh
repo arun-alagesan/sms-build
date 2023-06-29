7f4d8d86d3aca53076ee9ad1f3faad07c2cc7ac9
RekhaDinesh <117529319+RekhaDinesh@users.noreply.github.com>
2023-06-29 22:32:46 +0530

    Merge the changes for Background Task

--- checkout-code.sh
+++ checkout-code.sh
@@ -9,10 +9,10 @@ sleep 5
 mkdir $SOURCE_FOLDER
 cd $SOURCE_FOLDER
 echo $SMS_API_SOURCE
-git clone  -b main --single-branch  $SMS_API_SOURCE 
+git clone  -b main --depth 1 --single-branch  $SMS_API_SOURCE sms-spacemgmt-ms
 echo $SMS_UI_SOURCE
-git clone  -b master --single-branch  $SMS_UI_SOURCE 
+git clone  -b main --depth 1 --single-branch  $SMS_UI_SOURCE pixelKube
 echo $CAL_API_SOURCE
-git clone  -b main --single-branch  $CAL_API_SOURCE 
+git clone  -b master --depth 1 --single-branch  $CAL_API_SOURCE CalendarService
 cd ..
 
